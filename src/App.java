95e00775f3c7c3d67440af297def37eb761257fe
PauloEduardoS <70494617+PauloEduardoS@users.noreply.github.com>
2023-11-26 18:26:26 -0300

    Delete src directory

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import contaBanco.ContaTerminal;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        
-        ContaTerminal conta = new ContaTerminal();
-        
-    }
-}
